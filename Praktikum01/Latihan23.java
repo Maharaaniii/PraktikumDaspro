cb4fdb1995d066b10219d0119b9d7bdd7e5d5a98
maharaaniii <umimaharani08@gmail.com>
2024-09-06 09:36:53 +0700

    commit praktikum 01

---
+++ Praktikum01/Latihan23.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan23
+ */
+public class Latihan23 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Umi Maharani");
+        //System.out.println("\nsaya adalah Mahasiswa baru Prodi Sistem Informasi Bisnis-POLINEMA"); 
+    }
+}
---
+++ Praktikum01/tugas.java
@@ -0,0 +1,15 @@
+/**
+ * Latihan23
+ */
+public class tugas {
+
+    public static void main(String[] args) {
+        System.out.println("***IDENTITAS DIRI***");
+        System.out.println("Nama    : Umi Maharani");
+        System.out.println("Absen   : 23");
+        System.out.println("Kelas   : 1D");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+     
+    }
+}
